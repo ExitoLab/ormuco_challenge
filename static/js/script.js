17fcca19879316a3a7aeeb3f4e1e856fbfef00ae
Adetokunbo Ige <Adetokunbo.Ige@Interswitchng.com>
2019-04-30 17:51:44 +0100

    Modified the javascript, for more efficiency

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,20 @@
-$(function(){
+$(document).ready(function() {
 
-	$( "#name" ).blur(function() {
-		var name =  $('#name').val();
+	$("#pet_name" ).blur(function() {
+
+		if($('#pet_name').val() == ''){
+			alert('The pet name cannot be empty');
+			$('#pet_name').focus();
+			return false
+		}
+
 		$.ajax({
-			url: '/checkPetName',
-			data: $('form').serialize(),
-			type: 'POST',
+			url: '/check_pet_name',
+			type: 'GET',
+			dataType: 'json', // returning data as json
+			data: {
+				    pet_name:$('#pet_name').val()
+		          },
 			success: function(response){
 				console.log(response);
 			},
@@ -15,5 +24,30 @@ $(function(){
 		});
 	});
 
-	
-});
+	$('#register').click(function(event) {
+	  /* Act on the event */
+
+	if($('#pet_name').val() == ''){
+		alert('The pet name cannot be empty');
+		return false
+	}
+
+	if($('#pet_favorite_color').val() == ''){
+		alert('The pet favorite color cannot be empty');
+		return false
+	}
+
+	  $.ajax({
+		url: '/register_pet', //server url
+		type: 'POST',    //passing data as post method
+		dataType: 'json', // returning data as json
+		data: {pet_name:$('#pet_name').val(),pet_favorite_color:$('#pet_favorite_color').val(),pet_category: $('#pet_category').val()},  //form values
+		success:function(json)
+		{
+		  alert(json.result);  //response from the server given as alert message
+
+		}
+
+	  });
+	});
+  });
