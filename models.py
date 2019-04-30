a129fe76cdce59c8cd92a6b2ad7af5f97bbc0ae3
Adetokunbo Ige <Adetokunbo.Ige@Interswitchng.com>
2019-04-30 17:54:42 +0100

    Model - This interact with the database

---
+++ models.py
@@ -0,0 +1,17 @@
+import flask_sqlalchemy
+
+db = flask_sqlalchemy.SQLAlchemy()
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pet_name = db.Column(db.String(100))
+    pet_favorite_color = db.Column(db.String(20))
+    pet_category = db.Column(db.String(20))
+
+    def __init__(self, pet_name, pet_favorite_color, pet_category):
+        self.pet_name = pet_name
+        self.pet_favorite_color = pet_favorite_color
+        self.pet_category = pet_category
+
