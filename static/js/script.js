25e33781b6ba08d0e9deabce657fb26bda06090d
Adetokunbo Ige <Adetokunbo.Ige@Interswitchng.com>
2019-05-01 22:16:42 +0100

    Add more functionality to the javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
 
 	$("#pet_name" ).blur(function() {
-
 		if($('#pet_name').val() == ''){
-			alert('The pet name cannot be empty');
+			$('#pageMessage').html("The pet name cannot be empty");
 			$('#pet_name').focus();
 			return false
 		}
+
+		$("#search").prop('disabled', false);
 
 		$.ajax({
 			url: '/check_pet_name',
 			type: 'GET',
 			dataType: 'json', // returning data as json
-			data: {
-				    pet_name:$('#pet_name').val()
-		          },
+			data: {	pet_name:$('#pet_name').val()},
+			beforeSend: function() {
+				$("#search").prop('disabled', false);
+				$('#pageMessage').html("");
+			},
 			success: function(response){
-				console.log(response);
+				if (response.status == "Found") {
+					$('#pageMessage').html("Your name exist in the database, pls select another name");
+					$("#register").prop('disabled', true);
+					$('#pet_name').focus();
+					return false
+				}else {
+					$("#search").prop('disabled', false);
+					$('#pageMessage').html("");
+				}
 			},
 			error: function(error){
 				console.log(error);
@@ -25,29 +36,37 @@ $(document).ready(function() {
 	});
 
 	$('#register').click(function(event) {
-	  /* Act on the event */
 
-	if($('#pet_name').val() == ''){
-		alert('The pet name cannot be empty');
-		return false
-	}
+		$('#pageMessage').html("");
+		if($('#pet_name').val() == ''){
+			$('#pageMessage').html("The pet name cannot be empty");
+			$('#pet_name').focus();
+			return false
+		}
 
-	if($('#pet_favorite_color').val() == ''){
-		alert('The pet favorite color cannot be empty');
-		return false
-	}
+		if($('#pet_favorite_color').val() == ''){
+			$('#pageMessage').html("The pet favorite color cannot be empty");
+			$('#pet_favorite_color').focus();
+			return false
+		}
 
 	  $.ajax({
 		url: '/register_pet', //server url
 		type: 'POST',    //passing data as post method
 		dataType: 'json', // returning data as json
 		data: {pet_name:$('#pet_name').val(),pet_favorite_color:$('#pet_favorite_color').val(),pet_category: $('#pet_category').val()},  //form values
-		success:function(json)
+		success:function(response)
 		{
-		  alert(json.result);  //response from the server given as alert message
-
+			$('#pageMessage').html("");
+			if (response.status == "ok") {
+				$("#register input[type=text]").val('');
+				$('#pageMessage').html("Your registration was successful");
+				$("form").trigger("reset");
+			}
+		},
+		error: function(error){
+			console.log(error);
 		}
-
 	  });
 	});
   });
