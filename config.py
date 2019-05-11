2c0cb0346000ca8dc76edbd4036cd1eca5dc6343
Adetokunbo Ige <Adetokunbo.Ige@Interswitchng.com>
2019-05-12 00:23:55 +0100

    Change database name

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 user = "test"
 password = "password"
 host = "postgres"
-database = "ormuco_challenge"
+database = "example"
 port = 5432
 
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
